a04a82be4f7a13fa11ad2f61bb4b75246be122e2
Igor Bakhtin <ibakhtin@icloud.com>
2021-08-20 11:02:02 +0300

    Refactor App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-function App() {
-  return (
-      <div>
-        <h1>Hello Notedly!</h1>
-        <p>Welcome to the Notedly application</p>
-      </div>
-  );
-}
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Hello Notedly!</h1>
+    <p>Welcome to the Notedly application</p>
+  </div>
+);
+
 
 export default App;
