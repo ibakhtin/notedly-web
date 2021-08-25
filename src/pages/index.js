77d79e95ab42073a2b014e5c1c9d58a1eed7b645
Igor Bakhtin <ibakhtin@icloud.com>
2021-08-25 11:27:53 +0300

    add src/pages/note.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './home'
 import MyNotes from './mynotes'
 import Favorites from './favorites'
+import NotePage from './note'
 
 import Layout from '../components/Layout'
 
 const Pages = () => {
-    return (
-        <Router>
-            <Layout>
-                <Route exact path="/" component={Home}/>
-                <Route path="/mynotes" component={MyNotes}/>
-                <Route path="/favorites" component={Favorites}/>
-            </Layout>
-        </Router>
-    )
+  return (
+    <Router>
+      <Layout>
+        <Route exact path="/" component={ Home }/>
+        <Route path="/mynotes" component={ MyNotes }/>
+        <Route path="/favorites" component={ Favorites }/>
+        <Route path="/note/:id" component={ NotePage }/>
+      </Layout>
+    </Router>
+  )
 }
 
 export default Pages
