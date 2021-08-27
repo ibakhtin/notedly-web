7fcad64537bf43003b3b42342d8e88577057dc3e
Igor Bakhtin <ibakhtin@icloud.com>
2021-08-28 00:39:45 +0300

    refactor Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,35 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components'
+
+const outline = css`
+  color: #666;
+  background-color: transparent;
+  
+  &:hover {
+    color: #fcfcfc;
+    background-color: #666;
+  }
+`
+
+const round = css`
+  padding: 4px 18px 6px;
+  border-radius: 500px;
+`
 
 const Button = styled.button`
-  display: block;
-  padding: 0.5em 1.25em;
-  border-width: 0;
-  border-radius: 500px;
-  //font-weight: bold;
+  padding: 4px 14px 6px;
+  min-width: 5em;
+  border: 2px solid #666;
+  border-radius: 6px;
   color: #fcfcfc;
-  background-color: rgb(130, 132, 140);
+  background-color: #666;
   cursor: pointer;
-  box-shadow: 0 0 1em rgba(0, 0, 0, 0.25);
-
-
+  
   :hover {
-    
+    background-color: #555;
   }
-`;
+  
+  ${props => props.outline && outline}
+  ${props => props.round && round}
+`
 
-export default Button;
+export default Button
