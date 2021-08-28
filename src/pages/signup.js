7265d5174677ad1ad40d03be4ea9fabdf660c65a
Igor Bakhtin <ibakhtin@icloud.com>
2021-08-28 19:53:59 +0300

    add sign up page

---
+++ src/pages/signup.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useMutation, useApolloClient, gql } from '@apollo/client'
+
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+const Wrapper = styled.div`
+  border: 1px solid #f5f4f0;
+  max-width: 500px;
+  padding: 1em;
+  margin: 0 auto;
+`
+
+const Form = styled.form`
+  label,
+  input {
+    display: block;
+  }
+
+  label {
+    font-size: 14px;
+    margin-bottom: 0.33em;
+  }
+
+  input {
+    margin-bottom: 1em;
+  }
+`
+
+const SignUp = (props) => {
+
+  useEffect(() => {
+    document.title = 'Sign Up — Notedly'
+  })
+
+  const [values, setValues] = useState({ username: '', email: '', password: '' })
+
+  const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!) {
+      signUp(email: $email, username: $username, password: $password)
+    }
+  `
+
+  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+    onCompleted: data => {
+      localStorage.setItem('Token', data.signUp)
+      props.history.push('/')
+    }
+  })
+
+  const onChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    signUp({ variables: { ...values } })
+    setValues({ username: '', email: '', password: '' })
+  }
+
+  return (
+    <Wrapper>
+      <h2>Sign Up</h2>
+      <Form onSubmit={onSubmit}>
+        <label htmlFor="username">Username:</label>
+        <Input
+          required
+          value={values.username}
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+          onChange={onChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <Input
+          required
+          value={values.email}
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <label htmlFor="password">Password:</label>
+        <Input
+          required
+          value={values.password}
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+        <Button type="submit">Sign Up</Button>
+      </Form>
+    </Wrapper>
+  )
+}
+
+export default SignUp
