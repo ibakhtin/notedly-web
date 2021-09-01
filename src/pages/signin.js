dd830584c9ae83e760f503d973b415558fa7c55e
Igor Bakhtin <ibakhtin@icloud.com>
2021-09-01 08:56:36 +0300

    Add src/pages/signin.js file.

---
+++ src/pages/signin.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+const SignIn = () => {
+  useEffect(() => {
+    document.title = 'Sign In — Notedly'
+  })
+
+  return (
+    <div>
+      <p>Sign in page</p>
+    </div>
+  )
+}
+
+export default SignIn
