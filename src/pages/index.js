9aa257757efa5e955a3ab5f6538034fc3148504b
Igor Bakhtin <ibakhtin@icloud.com>
2021-09-02 21:35:17 +0300

    Add private routes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
 
 import Home from './home'
 import MyNotes from './mynotes'
@@ -7,19 +8,42 @@ import Favorites from './favorites'
 import NotePage from './note'
 import SignIn from '../components/SignIn'
 import SignUp from '../components/SignUp'
-
 import Layout from '../components/Layout'
+
+import { IS_LOGGED_IN } from '../graphql/query'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { data, loading, error } = useQuery(IS_LOGGED_IN)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        data.isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/signin', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  )
+}
 
 const Pages = () => {
   return (
     <Router>
       <Layout>
-        <Route exact path="/" component={ Home }/>
-        <Route path="/mynotes" component={ MyNotes }/>
-        <Route path="/favorites" component={ Favorites }/>
-        <Route path="/note/:id" component={ NotePage }/>
-        <Route path="/signin" component={ SignIn }/>
-        <Route path="/signup" component={ SignUp }/>
+        <Route exact path="/" component={Home}/>
+        <PrivateRoute path="/mynotes" component={MyNotes}/>
+        <PrivateRoute path="/favorites" component={Favorites}/>
+        <Route path="/note/:id" component={NotePage}/>
+        <Route path="/signin" component={SignIn}/>
+        <Route path="/signup" component={SignUp}/>
       </Layout>
     </Router>
   )
