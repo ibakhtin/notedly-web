b08ea130c1ad2f19acddae94d80578783aeab4af
Igor Bakhtin <ibakhtin@icloud.com>
2021-09-04 08:14:15 +0300

    Add ADD_NOTE mutation.

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -5,8 +5,28 @@ export const SIGNUP_USER = gql`
     signUp(email: $email, username: $username, password: $password)
   }
 `
+
 export const SIGNIN_USER = gql`
     mutation signIn($email: String!, $password: String!) {
       signIn(email: $email, password: $password)
     }
+`
+
+export const ADD_NOTE = gql`
+  mutation newNote($content: String!) {
+    newNote(content: $content) {
+      id
+      content
+      createdAt
+      favoriteCount
+      favoritedBy {
+        id
+        username
+      }
+      author {
+        id
+        username
+      }
+    }
+  } 
 `
